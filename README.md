31933a55da2d28ce92c5b3b07b31380376ad9622
zhengzhang <zhengzhang@ruijie.com.cn>
2019-03-27 14:16:48 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# python3" 
+http://note.youdao.com/noteshare?id=64d8d22b726d2a31c8e1507dd52886fb&sub=31FD2339528F42F4864C1531E9C2BDB4
---
+++ request.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# -*- coding:UTF-8 -*-
+import requests
+ 
+if __name__ == '__main__':
+    target = 'http://www.biqukan.com/1_1094/5403177.html'
+    req = requests.get(url=target)
+    print(req.text)
