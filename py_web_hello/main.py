8ee92e0b4e2409d5568c636fc0a19baf0f3de328
zhengzhang <zhengzhang@ruijie.com.cn>
2019-03-28 20:12:21 +0800

    2019-3-28

---
+++ py_web_hello/main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+ 
+ 
+app = Flask(__name__)
+ 
+ 
+@app.route('/index')
+def hello_world():
+    return "HelloWorld"
+ 
+if __name__ == "__main__":
+    app.run()
+
+
+
+
+
+
+
+
